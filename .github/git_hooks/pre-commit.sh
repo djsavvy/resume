8016631e22da6649b8e647ea2f655851a22f768b
djsavvy <sraghuvanshi@college.harvard.edu>
2020-06-23 16:57:03 -0400

    Fix pre-commit hook stashing behavior. NOTE: NOT MULTITHREAD SAFE

--- .github/git_hooks/pre-commit.sh
+++ .github/git_hooks/pre-commit.sh
@@ -10,8 +10,8 @@
 # (derived from: https://codeinthehole.com/tips/tips-for-using-a-git-pre-commit-hook/)
 STASH_NAME="pre-commit-$(date +%s)"
 echo "----------- Running pre-commit hook -----------"
-echo "Stashing unsaved changes in ${STASH_NAME}"
-git stash push -q --keep-index -m $STASH_NAME
+echo "Stashing unsaved changes in ${STASH_NAME}:"
+git stash push --keep-index -m $STASH_NAME
 
 # Actually build
 echo "Building: "
@@ -22,6 +22,6 @@ echo "Done building."
 
 # Restore unsaved changes
 echo "Attemping to restore stashed unsaved changes: popping stash ${STASH_NAME}..."
-git stash pop $STASH_NAME
+git stash pop
 
 echo "----------- Finished pre-commit hook -----------"
