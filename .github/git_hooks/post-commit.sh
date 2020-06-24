a019edf408b22e5733f826306afbafdd15ce9962
djsavvy <sraghuvanshi@college.harvard.edu>
2020-06-23 22:53:09 -0400

    Remove git hooks, instead building on server

--- .github/git_hooks/post-commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Usage:
-#   chmod u+x .github/git_hooks/post-commit.sh
-#   cd .git/hooks
-#   ln -s ../../.github/git_hooks/post-commit.sh ./post-commit
-
-git restore docs/build.css
